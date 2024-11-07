719b44af8806e85785d45369257f125c9e7335b1
enoc <enoc.alejandro.castrolobo@gmail.com>
2024-11-06 23:34:42 -0300

    cap 9_1° grupos de rutas

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,7 +20,6 @@ export async function fetchRevenue() {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
      console.log('Data fetch completed after 3 seconds.');
-
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
