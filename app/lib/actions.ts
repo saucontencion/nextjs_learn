b79aed007a9f4794cdef008b499b329055e9421a
enoc <enoc.alejandro.castrolobo@gmail.com>
2024-11-12 22:21:27 -0300

    13 manejo de errore, status 'no se encuentra la factura'

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,14 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-
+function handleErrorController (error) {
+  console.log(error);
+  
+  return {msg:`error en el actions : ${error}`,
+          status:500,
+          data:[]          
+}
+}
  
 const FormSchema = z.object({
     id: z.string(),
@@ -24,10 +31,14 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    try {
+      await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;  
+    } catch (error) {
+      handleErrorController(error)
+    }
    revalidatePath('/dashboard/invoices');
    redirect('/dashboard/invoices');
 
@@ -50,18 +61,27 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return handleErrorController(error)
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice' };
+
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice' };
+  }
 }
